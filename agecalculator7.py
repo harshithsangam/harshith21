864b3afe2bffdf03ad94fd9a193191bcc2600111
harshithsangam <115769109+harshithsangam@users.noreply.github.com>
2022-10-28 09:09:24 +0530

    Add files via upload

---
+++ agecalculator7.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import datetime
+    
+def ageCalculator():
+    today = datetime.datetime.now().date()
+    age = int((today - dob).date/365.25)
+    print("You are", age, " years old!")
+    
+def main():
+    st.title(' ofir Age Calculator')
+    st.write("Welcome to the Age Calculator!")
+    dob = st.date_input("Please enter your Birth Date:")
+    ageCalculator()
+    
+main()
